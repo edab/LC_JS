bdf01f0da8454d6e236fe9a33101a9a64a714471
edab <ing.dabramo@gmail.com>
2022-01-29 09:55:18 +0100

    feat: add day 2 problems algo I study plan

--- README.md
+++ README.md
@@ -10,4 +10,4 @@ My solution for LeetCode problems using JavaScript.
 | 278 | 1 | [First Bad Version](problems/LC278.js) | Easy
 | 35  | 1 | [Search Insert Position](problems/LC35.js) | Easy
 | 977 | 2 | [Squares of a Sorted Array](problems/LC977.js) | Easy
-
+| 189 | 2 | [Rotate Array*](problems/LC189.js) | Medium
---
+++ problems/LC189.js
@@ -0,0 +1,53 @@
+/**
+ * #189 - Rotate Array
+ *
+ * Given an array, rotate the array to the right by k steps,
+ * where k is non-negative.
+ *
+ * Difficulty: Medium
+ * Time Submitted: 01/30/2022 19:57
+ *
+ * Notes
+ * The idea is to rotate the array three times: the first the
+ * enteire array, the second only the first half, the third the
+ * remaining part.
+ * [1,2,3,4,5,6,7], k=3
+ * Step 1: [7,6,5,4,3,2,1]
+ * Step 2: [5,6,7 - 4,3,2,1]
+ * Step 3: [5,6,7 - 1,2,3,4]
+ * Result -> [5,6,7,1,2,3,4]
+ *
+ * Submission details
+ *   - Runtime: 151 ms, faster than 40.34% of other JS submissions
+ *   - Memory: 52.9 MB, less than 5.00% of other JS submissions
+ *
+ * Complexity analysis
+ *   - O(N) time | where N is the number of array elements
+ *   - O(1) space | where N is the number of array elements
+ *
+ */
+
+ /**
+  * @param {number[]} nums
+  * @param {number} k
+  * @return {void} Do not return anything, modify nums in-place instead.
+  */
+ var rotate = function(nums, k) {
+   const n = nums.length;
+   k = k % n;
+
+   function reverse_array(array, start, end) {
+     while(start < end) {
+       let temp = array[start];
+       array[start] = array[end];
+       array[end] = temp;
+       start++;
+       end--;
+     }
+   }
+
+   reverse_array(nums, 0, n - 1);
+   reverse_array(nums, 0, k - 1);
+   reverse_array(nums, k, n - 1);
+
+ };
